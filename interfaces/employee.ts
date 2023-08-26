40c90f9d290d766630ef9fd4be8c8044815985f2
Cristian <cquilaguy@unal.edu.co>
2023-08-26 17:20:56 -0500

    Interface

---
+++ interfaces/employee.ts
@@ -0,0 +1,11 @@
+export interface Employee{
+    fullName: string;
+    cc:number;
+    age:number;
+    position:string;
+    email:string;
+    phone:number;
+
+    //password:string;
+    //idProccess:number;
+}
