9f838f3017252bbd9c769a39ef9d692edfc23949
Cristian <cquilaguy@unal.edu.co>
2023-09-09 11:40:56 -0500

    delete a console log

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -2,8 +2,6 @@ import { Request,Response,Router } from "express";
 import {registerCtrl, loginCtrl} from "../controllers/auth"
 
 const router = Router()
-
-console.log('q pasa')
 /*http://localhost:4000/auth/login [POST] */
 router.post("/register", registerCtrl);
 router.post("/login", loginCtrl);
