1dea28db3cf7791a66b311ec700a5bce8c9ee27b
Cristian <cquilaguy@unal.edu.co>
2023-10-09 12:36:37 -0500

    Uploaded in Cloudinary, fix the visualization

--- src/controllers/generatePDF.ts
+++ src/controllers/generatePDF.ts
@@ -1,22 +1,40 @@
-import { Request, Response, response } from "express"
+import { Request, Response } from "express";
 import generatePdf from "../utils/generatePDF";
-import { handleHttp } from "../utils/error.handle"
+import { handleHttp } from "../utils/error.handle";
+import { uploadImage, deleteImage } from "../config/cloudinaryPDF";
+import * as fs from 'fs';
+import * as os from 'os';
+import * as path from 'path';
 
 const getPDF = async (req: Request, res: Response) => {
   try {
-    const pdfBuffer = await generatePdf(req.body.url);
     if (!req.body.url) {
         return res.status(400).send("URL is required");
     }
-    res.status(200).set({
-        "Access-Control-Allow-Origin":"*",
-        "Access-Control-Allow-Credentials":true,
-        "Content-Type":"application/pdf",
-    })
-    .end(pdfBuffer);
+
+    const pdfBuffer = await generatePdf(req.body.url);
+
+    // Crear un archivo temporal para el PDF
+    const tempFileName = 'tempPdf-' + Date.now() + '.pdf';
+    const tempFilePath = path.join(os.tmpdir(), tempFileName);
+    fs.writeFileSync(tempFilePath, pdfBuffer);
+
+    const uploadResult = await uploadImage(tempFilePath);
+    fs.unlinkSync(tempFilePath);  // Eliminar el archivo temporal después de cargarlo
+
+    if (!uploadResult || !uploadResult.url) {
+      throw new Error("Failed to upload PDF to Cloudinary");
+    }
+
+    // Responder con la URL del PDF en Cloudinary
+    res.status(200).json({
+      message: "PDF uploaded successfully!",
+      url: uploadResult.url
+    });
+
   } catch (e) {
     handleHttp(res,'ERROR_CREATE_PDF');
   }
 };
 
-export {getPDF};
+export { getPDF };
